18b3d15475724fbf2b282333a4d8399cc1ccaca1
Viraj 😄 <180491776+iViraj@users.noreply.github.com>
2026-03-01 18:52:20 +0530

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,23 @@
+function initMap(){
+
+const map = document.getElementById("mapContainer");
+
+map.innerHTML = `
+
+<h2>Game Zones</h2>
+
+<div class="zone">
+🏢 City Center
+</div>
+
+<div class="zone">
+🏭 Factory
+</div>
+
+<div class="zone">
+🌆 Downtown
+</div>
+
+`;
+
+}
